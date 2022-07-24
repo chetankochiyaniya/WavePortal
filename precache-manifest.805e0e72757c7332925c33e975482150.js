1eb019cbdbcaa79bb5465d7729b2a5dc04bc36fb
Chetan Kochiyaniya <ckochiyaniya950@rku.ac.in>
2022-07-25 01:03:27 +0530

    Updates

---
+++ precache-manifest.805e0e72757c7332925c33e975482150.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e0683dc6847ab18c89b4002d469264c",
+    "url": "/waveportal/index.html"
+  },
+  {
+    "revision": "7e8dbe4f6d033dac774d",
+    "url": "/waveportal/static/css/main.233cbc38.chunk.css"
+  },
+  {
+    "revision": "5bb3135748fadc4c1b95",
+    "url": "/waveportal/static/js/2.2f1b886b.chunk.js"
+  },
+  {
+    "revision": "7e8dbe4f6d033dac774d",
+    "url": "/waveportal/static/js/main.b5726d5f.chunk.js"
+  },
+  {
+    "revision": "aef81ca1c527b8d66c72",
+    "url": "/waveportal/static/js/runtime-main.4df9bcaa.js"
+  }
+]);
